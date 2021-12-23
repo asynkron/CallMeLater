49244658689e506fb948e29816c11d9b07dd3b6c
Roger Johansson <rogeralsing@gmail.com>
2021-12-23 14:42:53 +0100

    .

--- main.go
+++ main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
+	//X-Later-Request-Url 	- url to call
+	//X-Later-When 			- UTC timestamp
+	//X-Later-Response-Url 	- webhook to send results to
+
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
 func handleRequests() {
-	rtr := mux.NewRouter()
-	rtr.HandleFunc(`/later/{rest:.+/}`, homePage)
-	http.Handle("/", rtr)
+	http.HandleFunc("/later", homePage)
 	err := http.ListenAndServe(":10000", nil)
 	log.Fatal(err)
 }
