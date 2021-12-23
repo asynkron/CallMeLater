400f3ff91fcce7ebdc5d8a861e90ad1d0f9bf96b
Roger Johansson <rogeralsing@gmail.com>
2021-12-23 14:22:26 +0100

    .

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/number/{id:[0-9]+}", homePage)
+	http.HandleFunc("/later/{path:[0-9]+}", homePage)
+	err := http.ListenAndServe(":10000", nil)
+	log.Fatal(err)
+}
 
 func main() {
-	fmt.Println("hello")
+	handleRequests()
 }
