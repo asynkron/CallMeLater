c3a1cc234639fe223d9e89e919319471b08d4550
Roger Johansson <rogeralsing@gmail.com>
2021-12-23 14:50:02 +0100

    .

--- main.go
+++ main.go
@@ -4,12 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"time"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	//X-Later-Request-Url 	- url to call
+	requestUrl, err := url.Parse(r.Header.Get("X-Later-Request-Url"))
+	if err != nil {
+		return
+	}
 	//X-Later-When 			- UTC timestamp
+	layout := "2006-01-02 15:04:05 -0700 MST"
+	time, err := time.Parse(layout, r.Header.Get("X-Later-When"))
+	if err != nil {
+		return
+	}
 	//X-Later-Response-Url 	- webhook to send results to
+	responseUrl, err := url.Parse(r.Header.Get("X-Later-Response-Url"))
+	if err != nil {
+		return
+	}
 
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
