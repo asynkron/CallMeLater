aa396e575cce9be7154fafd75ed548fd6799e52a
Roger Johansson <rogeralsing@gmail.com>
2021-12-24 18:58:06 +0100

    docker compose

--- api.go
+++ api.go
@@ -91,6 +91,7 @@ func saveRequest(rd *requestData) {
 	log.
 		Info().
 		Str("Json", json).Msg("Saved Request")
+
 	requests <- rd
 }
 
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"os"
+	"sort"
 )
 
 var (
@@ -12,13 +13,24 @@ var (
 )
 
 func consumeLoop() {
+	var pendingRequests []*requestData
+
 	log.
 		Info().
 		Msg("Consume loop started")
 
 	for {
 		rd := <-requests
-		go sendRequestResponse(rd)
+
+		pendingRequests = append(pendingRequests, rd)
+
+		sort.Slice(pendingRequests, func(i, j int) bool {
+			w1 := pendingRequests[i].When
+			w2 := pendingRequests[j].When
+			return w1.Before(w2)
+		})
+
+		pendingRequests = pendingRequests[0:100]
 	}
 }
 
