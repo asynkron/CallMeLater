0a58599eb6d612b3ab514d48f7eaa88c5526b39d
Roger Johansson <rogeralsing@gmail.com>
2021-12-25 18:43:30 +0100

    restructure

--- server/worker.go
+++ server/worker.go
@@ -44,47 +44,32 @@ func (w *worker) run() {
 		select {
 		case rd := <-w.requests:
 
-			w.pending = append(w.pending, rd)
-
-			sort.Slice(w.pending, func(i, j int) bool {
-				w1 := w.pending[i].When
-				w2 := w.pending[j].When
-				return w1.Before(w2)
-			})
+			w.appendRequest(rd)
 
 			log.
 				Info().
 				Int("QueueLength", len(w.pending)).
 				Msg("Worker received new request")
 
-			err = w.sendExpiredRequests()
-			if err != nil {
-				log.
-					Err(err).
-					Msg("failed to send expired requests")
-				return
-			}
-
-			if len(w.pending) > 100 {
-				w.pending = w.pending[0:100]
-				w.hasMore = true
-			}
-
 		case <-time.After(time.Second):
 			log.
 				Info().
 				Int("QueueLength", len(w.pending)).
 				Msg("Worker received no new messages")
-
-			err = w.sendExpiredRequests()
-			if err != nil {
-				log.
-					Err(err).
-					Msg("failed to send expired requests")
-				return
-			}
 		}
+		_ = w.sendExpiredRequests()
+		_ = w.loadMoreRequests()
 	}
+}
+
+func (w *worker) appendRequest(rd *RequestData) {
+	w.pending = append(w.pending, rd)
+
+	sort.Slice(w.pending, func(i, j int) bool {
+		w1 := w.pending[i].When
+		w2 := w.pending[j].When
+		return w1.Before(w2)
+	})
 }
 
 func (w *worker) sendExpiredRequests() error {
@@ -105,12 +90,15 @@ func (w *worker) sendExpiredRequests() error {
 		}
 	}
 
-	err := w.loadMore()
+	if len(w.pending) > 100 {
+		w.pending = w.pending[0:100]
+		w.hasMore = true
+	}
 
-	return err
+	return nil
 }
 
-func (w *worker) loadMore() error {
+func (w *worker) loadMoreRequests() error {
 	if len(w.pending) > 0 {
 		return nil
 	}
@@ -125,6 +113,11 @@ func (w *worker) loadMore() error {
 
 	pr, err := w.storage.Pull(w.pullCount)
 	if err != nil {
+		if err != nil {
+			log.
+				Err(err).
+				Msg("Error loading more")
+		}
 		return err
 	}
 	w.pending = pr
