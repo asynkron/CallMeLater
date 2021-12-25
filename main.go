12d19949a6d22f5d39278d58dd329de118b22172
Thomas Ornell <thor@abax.no>
2021-12-25 15:43:30 +0100

    Adding support for sqlite

--- main.go
+++ main.go
@@ -17,7 +17,11 @@ func main() {
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
+	// PSQL
 	storage = newPgStorage("postgres://postgres:postgres@localhost:5432/callme?sslmode=disable")
+	// SQLite
+	storage = newSqLiteStorage("file:storage.db?cache=shared&mode=memory")
+
 	go consumeLoop()
 	handleRequests()
 }
