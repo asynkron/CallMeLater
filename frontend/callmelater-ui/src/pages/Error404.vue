d426fa5933d4b420fef5e9180369a1b89fe919ca
Roger Johansson <rogeralsing@gmail.com>
2021-12-28 17:08:41 +0100

    adding frontend

---
+++ frontend/callmelater-ui/src/pages/Error404.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+    <div>
+      <div style="font-size: 30vh">
+        404
+      </div>
+
+      <div class="text-h2" style="opacity:.4">
+        Oops. Nothing here...
+      </div>
+
+      <q-btn
+        class="q-mt-xl"
+        color="white"
+        label="Go Home"
+        no-caps
+        text-color="blue"
+        to="/"
+        unelevated
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'Error404'
+})
+</script>
---
+++ frontend/callmelater-ui/src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+        alt="Quasar logo"
+        src="~assets/quasar-logo-vertical.svg"
+        style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  setup() {
+    console.log("Hello from PageIndex");
+  }
+})
+</script>
